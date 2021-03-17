cbb25dd94c784fdc179ce6af3d7bdd92793358a9
Patrykshub <63500126+Patrykshub@users.noreply.github.com>
2021-03-17 17:20:28 +0000

    feat: Some work has been done

---
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import classes from './BurgerIngredient.css';
+
+const burgerIngredeint = (props) => {
+    let ingredeint = null;
+
+    switch (props.type) {
+        case ('bread-bottom'):
+            ingredient = <div className={}></div>;
+            break;
+        case ('bread-top')    
+    }
+};
+
+export default burgerIngreient;
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,13 +1,14 @@
 import React from 'react';
-
+import classes from './Layout.css';
 import Aaux from '../../hoc/Aaux';
 
 const layout = (props) => (
     <Aaux>
         <div>Toolbar, SideDrawer, Backdrop</div>
-        <main>
+        <main className={classes.Content}>
             {props.children}
         </main>
+
     </Aaux>    
 );
 
