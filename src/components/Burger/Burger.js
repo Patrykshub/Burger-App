43c59c9f0a42dbd4e41b1d18727dff24ee264ced
Patrykshub <63500126+Patrykshub@users.noreply.github.com>
2021-03-24 22:25:04 +0000

    feat: 130 lekcja

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,13 +3,19 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
-    const transformedIngredients = Object.keys(props.ingredients)
+    let transformedIngredients = Object.keys(props.ingredients)
         .map(igKey => {
             return [...Array(props.ingredients[igKey])].map((_, i) => {
                return<BurgerIngredient key={igKey + i} type={igKey} />;
             }) // tworzy array o długości zależnej od ilości danego składnika
             // value objektu jest potrzebne do ilości jednego składnika a key jest do różnych składników
-        });
+        })
+        .reduce((arr, el) => {
+            return arr.concat(el)
+        }, []);
+        if (transformedIngredients.length === 0){
+            transformedIngredients = <p>Please start adding ingredients Madafaka!</p>
+        }
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
