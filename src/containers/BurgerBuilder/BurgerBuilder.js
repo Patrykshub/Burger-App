b50224e0877b50828828e39a2eba540bd3976139
Patrykshub <63500126+Patrykshub@users.noreply.github.com>
2021-03-26 17:46:30 +0000

    feat: Remove handler added

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -36,7 +36,17 @@ class BurgerBuilder extends Component {
     }
 
     removeIngredientHandler = (type) => {
+        const oldCount = this.state.ingredients[type];
+        const updatedCount = oldCount - 1;
+        const updatedIngredients = {
+            ...this.state.ingredients 
+        };
+        updatedIngredients[type] = updatedCount;
 
+        const priceDeduction = INGREDIENT_PRICES[type];
+        const oldPrice = this.state.totalPrice;
+        const newPrice = oldPrice - priceDeduction;
+        this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
     }
 
     render (){
@@ -44,7 +54,9 @@ class BurgerBuilder extends Component {
             <Aaux>
                <Burger ingredients={this.state.ingredients} /> 
                <BuildControls
-                    ingredientAdded={this.addIngredientHandler} />
+                    ingredientAdded={this.addIngredientHandler} 
+                    ingredientDeleted={this.removeIngredientHandler}    
+                    />
             </Aaux>
 
         );
