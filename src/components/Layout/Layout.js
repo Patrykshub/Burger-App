72ef2ee862ceaba986067375960c4fa53f7d908d
Patrykshub <63500126+Patrykshub@users.noreply.github.com>
2021-03-29 18:08:06 +0100

    feat: Toolbar

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import classes from './Layout.css';
 import Aaux from '../../hoc/Aaux';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
 
 const layout = (props) => (
     <Aaux>
+        <Toolbar></Toolbar>
         <div>Toolbar, SideDrawer, Backdrop</div>
         <main className={classes.Content}>
             {props.children}
