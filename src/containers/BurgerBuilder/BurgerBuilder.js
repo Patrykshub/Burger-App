92b1a975347b816c8b8ee17e2e47c4cc2dbff17f
Patrykshub <63500126+Patrykshub@users.noreply.github.com>
2021-04-06 17:46:05 +0100

    feat: sending a POST request

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,6 +5,9 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios.orders';
+
+
 const INGREDIENT_PRICES = {
     salad: 0.5,
     cheese: 0.4,
@@ -77,7 +80,25 @@ class BurgerBuilder extends Component {
         }
 
         purchaseContinueHandler = () => {
-            alert('You continue!');
+         //   alert('You continue!');
+         const order = {
+             ingredients: this.state.ingredients,
+             price: this.state.totalPrice,
+             customer: {
+                 name: 'Patryk B',
+                 address: {
+                     street: 'Teststreet 1',
+                     zipCode: '123123',
+                     country: 'Poland'
+                 },
+                 email: 'test@test.com'
+             },
+             deliveryMethod: 'fasttest'
+         }
+        axios.post('/orders.json', order)
+        .then(response => console.log(response))
+        .catch(error => console.log(error));
+        
         }
 
     render () {
