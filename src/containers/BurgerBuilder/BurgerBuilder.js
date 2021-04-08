120a11e3281cd1dc1b6e95fa5312ca1708bc2828
Patrykshub <63500126+Patrykshub@users.noreply.github.com>
2021-04-08 17:21:15 +0100

    feat:  Retrieving Data from the Backend

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -24,13 +24,17 @@ class BurgerBuilder extends Component {
         totalPrice: 4,
         purchasable: false,
         purchasing: false,
-        loading: false
+        loading: false,
+        error: false
     }
 
     componentDidMount () {
         axios.get('https://react-my-burger360-default-rtdb.firebaseio.com/ingredients.json')
         .then(response => {
             this.setState({ingredients: response.data});
+        })
+        .catch(error => {
+            this.setState({error: true})
         });
     }
 
@@ -122,7 +126,10 @@ class BurgerBuilder extends Component {
             disabledInfo[key] = disabledInfo[key] <= 0
         }
         let orderSummary = null;
-        let burger = <Spinner />;
+        if (this.state.loading) {
+            orderSummary = <Spinner />
+        }
+        let burger = this.state.error ? <p>Ingredients can't be loaded!</p> :<Spinner />;
         if (this.state.ingredients) {
             burger = (
                 <Aaux>
