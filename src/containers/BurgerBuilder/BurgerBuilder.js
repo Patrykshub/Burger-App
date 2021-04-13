d24466818b06d9c47416135e8d4832d3d7b48118
Patrykshub <63500126+Patrykshub@users.noreply.github.com>
2021-04-13 18:47:38 +0100

    feat: Passing ingredients via query Params

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -29,6 +29,8 @@ class BurgerBuilder extends Component {
     }
 
     componentDidMount () {
+        console.log(this.props);
+
         axios.get('https://react-my-burger360-default-rtdb.firebaseio.com/ingredients.json')
         .then(response => {
             this.setState({ingredients: response.data});
@@ -92,31 +94,40 @@ class BurgerBuilder extends Component {
         }
 
         purchaseContinueHandler = () => {
-         //   alert('You continue!');
-         this.setState({loading: true});
-         const order = {
-             ingredients: this.state.ingredients,
-             price: this.state.totalPrice,
-             customer: {
-                 name: 'Patryk B',
-                 address: {
-                     street: 'Teststreet 1',
-                     zipCode: '123123',
-                     country: 'Poland'
-                 },
-                 email: 'test@test.com'
-             },
-             deliveryMethod: 'fasttest'
-         }
-        axios.post('/orders.json', order)
-        .then(response => {
-            this.setState({loading: false, purchasing: false});
-        })
+        //   alert('You continue!');
+        //  this.setState({loading: true});
+        //  const order = {
+        //      ingredients: this.state.ingredients,
+        //      price: this.state.totalPrice,
+        //      customer: {
+        //          name: 'Patryk B',
+        //          address: {
+        //              street: 'Teststreet 1',
+        //              zipCode: '123123',
+        //              country: 'Poland'
+        //          },
+        //          email: 'test@test.com'
+        //      },
+        //      deliveryMethod: 'fasttest'
+        //  }
+        // axios.post('/orders.json', order)
+        // .then(response => {
+        //     this.setState({loading: false, purchasing: false});
+        // })
             
-        .catch(error => {
-            this.setState({loading: false, purchasing: false});
+        // .catch(error => {
+        //     this.setState({loading: false, purchasing: false});
         
-        });
+        // });
+            const queryParams = [];
+            for (let i in this.state.ingredients) {
+                queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
+            }
+            const queryString = queryParams.join('&');
+            this.props.history.push({
+                pathname: '/checkout',
+                search: '?' + queryString
+            });
     }
     render () {
         const disabledInfo = {
