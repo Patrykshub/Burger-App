58e30006935f39e6680399c9519c36b513100515
Patrykshub <63500126+Patrykshub@users.noreply.github.com>
2021-04-14 17:30:25 +0100

    feat: Adding an Orders page

---
+++ src/components/Order/Orders.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+import Order from '../../containers/Orders/Order';
+
+class Order extends Component {
+    render () {
+        return (
+            <div>
+                <Order />
+                <Order />
+            </div>
+
+
+        );
+    }
+}
+
+export default Order;
